5194313c78d30cb9f53ac406e0d5ccddf5ad0ecf
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-08-11 17:39:14 -0300

    Dashboard page styling

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,72 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/github-explorer-logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/42723081?s=460&u=fb5ef6722781663e80c6417824e4bb5059c5fcfd&v=4"
+            alt="Gustavo"
+          />
+          <div>
+            <strong>gpmarchi/doctors-clinic-backend</strong>
+            <p>
+              Projeto para portfólio de uma api REST para gerenciamento de
+              consultórios médicos.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/42723081?s=460&u=fb5ef6722781663e80c6417824e4bb5059c5fcfd&v=4"
+            alt="Gustavo"
+          />
+          <div>
+            <strong>gpmarchi/doctors-clinic-backend</strong>
+            <p>
+              Projeto para portfólio de uma api REST para gerenciamento de
+              consultórios médicos.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/42723081?s=460&u=fb5ef6722781663e80c6417824e4bb5059c5fcfd&v=4"
+            alt="Gustavo"
+          />
+          <div>
+            <strong>gpmarchi/doctors-clinic-backend</strong>
+            <p>
+              Projeto para portfólio de uma api REST para gerenciamento de
+              consultórios médicos.
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,7 +11,7 @@ export default createGlobalStyle`
 	}
 
 	body {
-		background: #F0F0F5 url(${githubBackground}) no-repeat 70% top;
+		background: #F0F0F5 url(${githubBackground}) no-repeat 62% -12%;
 		-webkit-font-smoothing: antialiased;
 	}
 
